09a22c1dbe30a56a398bbfed587d7ae62225bd13
Marco <m.spreckels@hotmail.de>
2021-01-16 23:04:10 +0100

    yes

--- git.lua
+++ git.lua
@@ -1,7 +1,7 @@
 local giturl = "https://github.com/MSpreckels/mc-turtles/blob/master/"
 
 function get(filename, path)
-    raw = http.get(giturl .. url .. ".lua").readAll()
+    raw = http.get(giturl .. filename .. ".lua").readAll()
     local file = fs.open(path, "w")
     file.write(data)
     file.flush()
