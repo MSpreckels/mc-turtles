2e550dd15cb00726414e97ade708ad57f91bd280
Marco <m.spreckels@hotmail.de>
2021-01-16 23:01:18 +0100

    yes

--- git.lua
+++ git.lua
@@ -1,5 +1,7 @@
-function get(url, path)
-    raw = http.get(url).readAll()
+local giturl = "https://github.com/MSpreckels/mc-turtles/blob/master/"
+
+function get(filename, path)
+    raw = http.get(giturl .. url .. ".lua").readAll()
     local file = fs.open(path, "w")
     file.write(data)
     file.flush()
--- startup.lua
+++ startup.lua
@@ -1,11 +1,23 @@
 --pastebin run fysWqF7B
 
+print("checking if git api exists...")
+if fs.exists("/met/common/git") then
+    print("folder met/common has been found, do you want to update it? y/n")
+    if read() == "y" then
+        shell.run("rm", "/met/common/git")
+    end
+else
+  shell.run("pastebin", "get", "iSCvBQQg", "/met/common/git")
+end
+
+os.loadAPI("/met/common/git")
+
 print("loading common apis..")
 
-if fs.exists("/common") then
-    print("folder common has been found, do you want to update it? y/n")
+if fs.exists("/met/common") then
+    print("folder met/common has been found, do you want to update it? y/n")
     if read() == "y" then
-        shell.run("rm", "/common")
+        shell.run("rm", "/met/common")
     end
 end
 
@@ -16,15 +28,16 @@ if fs.exists("/startup") then
     end
 end
 
-if not fs.exists("/common/json") then
-    shell.run("pastebin", "get", "5HHC17Xg", "/common/json")
+if not fs.exists("/met/common/json") then
+    -- shell.run("pastebin", "get", "5HHC17Xg", "/met/common/json")
+    git.get("json", "/met/common/json")
 end
 
 if turtle then
-    if fs.exists("/profile") then
+    if fs.exists("/met/profile") then
         print("profile folder detected, do you want to clean it? y/n")
         if read() == "y" then
-            shell.run("rm", "/profile")
+            shell.run("rm", "/met/profile")
         end
     end
 
@@ -32,33 +45,41 @@ if turtle then
         shell.run("rm", "/startup/customturtle")
     end
 
-    if not fs.exists("/common/transform") then
-        shell.run("pastebin", "get", "jLaR0VT0", "/common/transform")
+    if not fs.exists("/met/common/transform") then
+        -- shell.run("pastebin", "get", "jLaR0VT0", "/met/common/transform")
+        git.get("transform", "/met/common/transform")
+
     end
 
-    if not fs.exists("/common/profile") then
-        shell.run("pastebin", "get", "zjkdYmXV", "/common/profile")
+    if not fs.exists("/met/common/profile") then
+        -- shell.run("pastebin", "get", "zjkdYmXV", "/met/common/profile")
+        git.get("profile", "/met/common/profile")
     end
 
-    shell.run("pastebin", "get", "c18S5NEU", "/startup/customturtle")
+    -- shell.run("pastebin", "get", "c18S5NEU", "/startup/customturtle")
+    git.get("customturtle", "/startup/customturtle")
     shell.run("/startup/customturtle")
 else    
-    if not fs.exists("/common/names") then
-        shell.run("pastebin", "get", "qUfJSu3d", "/common/names")
+    if not fs.exists("/met/common/names") then
+        -- shell.run("pastebin", "get", "qUfJSu3d", "/met/common/names")
+        git.get("names", "/met/common/names")
     end
 
-    if not fs.exists("/common/utils") then
-        shell.run("pastebin", "get", "eYE7aKcL", "/common/utils")
+    if not fs.exists("/met/common/utils") then
+        -- shell.run("pastebin", "get", "eYE7aKcL", "/met/common/utils")
+        git.get("utils", "/met/common/utils")
     end    
 
-    if not fs.exists("/common/rest") then
-        shell.run("pastebin", "get", "ADSScD8t", "/common/rest")
+    if not fs.exists("/met/common/rest") then
+        -- shell.run("pastebin", "get", "ADSScD8t", "/met/common/rest")
+        git.get("rest", "/met/common/rest")
     end
 
     if fs.exists("/startup/hub") then
         shell.run("rm", "/startup/hub")
     end
     
-    shell.run("pastebin", "get", "XD2cLT0G", "/startup/hub")
+    -- shell.run("pastebin", "get", "XD2cLT0G", "/startup/hub")
+    git.get("hub", "/startup/hub")
     shell.run("/startup/hub")
 end
