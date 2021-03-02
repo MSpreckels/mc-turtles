986447930f7cf231817a4ab50bf08fdd27674d8a
Marco <m.spreckels@hotmail.de>
2021-03-02 20:02:36 +0100

    constructor

--- constructor.lua
+++ constructor.lua
@@ -1,20 +1,27 @@
-if turtle.getFuelLevel() < 1 then
-  turtle.select(16)
-  turtle.refuel(1)
+function construct(amount)
+  if turtle.getFuelLevel() < 12 then
+    turtle.select(16)
+    turtle.refuel(1)
+  end
+  
+  turtle.up()
+  turtle.select(14)
+  turtle.place()
+  turtle.select(15)
+  turtle.drop()
+  turtle.down()
+
+  for i = 1, amount, 1 do
+    turtle.select(1)
+    turtle.place()
+  end
+ 
+  turtle.up()
+  turtle.select(15)
+  turtle.suck()
+  turtle.select(14)
+  turtle.dig()
+  turtle.down()
 end
 
-turtle.up()
-turtle.select(14)
-turtle.place()
-turtle.select(15)
-turtle.drop()
-turtle.down()
-turtle.select(1)
-turtle.place()
-
-turtle.up()
-turtle.select(15)
-turtle.suck()
-turtle.select(14)
-turtle.dig()
-turtle.down()
+construct(10)
