26edece09b41a0d68338e9920679a8d0e82cbe2d
Marco <m.spreckels@hotmail.de>
2021-03-06 15:51:31 +0100

    farmhub

--- farmhub.lua
+++ farmhub.lua
@@ -4,19 +4,18 @@ local toggle = false
 monitor.clear()
 
 function drawbutton(buttonData, color)
-  for i = 1, buttonData.h, 1 do
-    monitor.setCursorPos(buttonData.x, i)
+  for i = 0, buttonData.h-1, 1 do
+    monitor.setCursorPos(buttonData.x, buttonData.y + i)
     monitor.setBackgroundColor(color)
     monitor.write(string.rep(" ", buttonData.w))
   end
 
-  monitor.setCursorPos(buttonData.x + buttonData.w / 2, buttonData.y + buttonData.h / 2)
+  monitor.setCursorPos(buttonData.x, buttonData.y)
   monitor.setTextColor(colors.white)
   monitor.write(buttonData.text)
 
   return button;
 end
-
 
 buttons = {}
 
@@ -25,13 +24,24 @@ button.id = 0;
 button.text = "Test";
 button.x = 2;
 button.y = 2;
-button.w = sizeX-1;
+button.w = sizeX-2;
 button.h = 1;
 button.onClick = nil;
 
+button2 = {}
+button2.id = 0;
+button2.text = "Test2";
+button2.x = 2;
+button2.y = 4;
+button2.w = sizeX-2;
+button2.h = 1;
+button2.onClick = nil;
+
 buttons[button.id] = button
+buttons[button2.id] = button2
 
 drawbutton(buttons[0], colors.green)
+drawbutton(buttons[1], colors.red)
 
 while true do
 
