3867b5d38af58305d045f2f926b536c0a4aa70f4
Marco <m.spreckels@hotmail.de>
2021-03-06 20:32:24 +0100

    mobfarm

--- farmturtle.lua
+++ farmturtle.lua
@@ -12,10 +12,32 @@ end
 
 rednet.send(hubID, '{"http":"post","endpoint":"addTurtle"}')
 
+
+function placeGrasBlock()
+  turtle.select(2)
+  turtle.digUp()
+  turtle.select(1)
+  turtle.placeUp()
+  redstone.setAnalogOutput("top", 0)
+end
+
+function placeRedstoneLamp()
+  turtle.select(1)
+  turtle.digUp()
+  turtle.select(2)
+  turtle.placeUp()
+  redstone.setAnalogOutput("top", 15)
+end
+
 while true do  
   id, res = rednet.receive(10)
   if res ~= nil then
     obj = json.decode(res)
-    print(obj.isFarmActive)
+    
+    if obj.isFarmActive then
+      placeGrasBlock()
+    else
+      placeRedstoneLamp()
+    end
   end
 end
