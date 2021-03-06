864a5b32729000e3225bd5afdf314a5cd5962380
Marco <m.spreckels@hotmail.de>
2021-03-06 13:09:39 +0100

    farmhub

--- farmhub.lua
+++ farmhub.lua
@@ -1,4 +1,5 @@
 local monitor = peripheral.wrap("front")
+local sizeX, sizeY = monitor.getSize()
 local toggle = false
 
 while true do
@@ -9,9 +10,15 @@ while true do
   if toggle then
     monitor.setBackgroundColor(colors.red)
     redstone.setAnalogOutput("back", 0)
+    
+    monitor.setCursorPos(1, sizeY / 2)
+    monitor.write("off");
   else
     monitor.setBackgroundColor(colors.green)
     redstone.setAnalogOutput("back", 1)
+
+    monitor.setCursorPos(1, sizeY / 2)
+    monitor.write("on");
   end
 
   monitor.clear()
